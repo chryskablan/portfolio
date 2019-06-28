0ad23f9a50f8607ed3948974d499ddb432354956
QUALIGROUP\christian.oupoh <christian.oupoh@qualiconsult.fr>
2019-06-28 16:40:23 +0200

    * Ajout du css pour les KPI pour les compétences     * Modification du contenue dans le fichier index     * Ajout de certaines photo pour le background     * Ajout d'un formulaire de contact     * Ajout d'un google maps pour me localiser     * Ajout des réseau sociaux +CHANGELOG

--- app/CHANGELOG.md
+++ app/CHANGELOG.md
@@ -1,3 +1,9 @@
 # ADD 
 - General 
     * Installation de laravel 5.8 + config de la base de donnée 
+    * Ajout du css pour les KPI pour les compétences 
+    * Modification du contenue dans le fichier index
+    * Ajout de certaines photo pour le background 
+    * Ajout d'un formulaire de contact 
+    * Ajout d'un google maps pour me localiser 
+    * Ajout des réseau sociaux 
--- app/resources/views/welcome.blade.php
+++ app/resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
   <meta name="description" content="">
   <meta name="author" content="">
 
-  <title>Stylish Portfolio - Start Bootstrap Template</title>
+  <title>Christian Oupoh Portfolio</title>
 
   <!-- Bootstrap Core CSS -->
   <link href="vendor/bootstrap/css/bootstrap.css" rel="stylesheet">
@@ -32,16 +32,16 @@
   <nav id="sidebar-wrapper">
     <ul class="sidebar-nav">
       <li class="sidebar-brand">
-        <a class="js-scroll-trigger" href="#page-top">Start Bootstrap</a>
+        <a class="js-scroll-trigger" href="#page-top">Menu</a>
       </li>
       <li class="sidebar-nav-item">
-        <a class="js-scroll-trigger" href="#page-top">Home</a>
+        <a class="js-scroll-trigger" href="#page-top">Accueil</a>
       </li>
       <li class="sidebar-nav-item">
-        <a class="js-scroll-trigger" href="#about">About</a>
+        <a class="js-scroll-trigger" href="#about">A Propos</a>
       </li>
       <li class="sidebar-nav-item">
-        <a class="js-scroll-trigger" href="#services">Services</a>
+        <a class="js-scroll-trigger" href="#services">Compétences</a>
       </li>
       <li class="sidebar-nav-item">
         <a class="js-scroll-trigger" href="#portfolio">Portfolio</a>
@@ -55,11 +55,11 @@
   <!-- Header -->
   <header class="masthead d-flex">
     <div class="container text-center my-auto">
-      <h1 class="mb-1">Stylish Portfolio</h1>
+      {{-- <h1 class="mb-1">Stylish Portfolio</h1>
       <h3 class="mb-5">
         <em>A Free Bootstrap Theme by Start Bootstrap</em>
-      </h3>
-      <a class="btn btn-primary btn-xl js-scroll-trigger" href="#about">Find Out More</a>
+      </h3> --}}
+      <a class="btn btn-primary btn-xl js-scroll-trigger" href="#about">Découvrez en plus</a>
     </div>
     <div class="overlay"></div>
   </header>
@@ -69,10 +69,12 @@
     <div class="container text-center">
       <div class="row">
         <div class="col-lg-10 mx-auto">
-          <h2>Stylish Portfolio is the perfect theme for your next project!</h2>
-          <p class="lead mb-5">This theme features a flexible, UX friendly sidebar menu and stock photos from our friends at
-            <a href="https://unsplash.com/">Unsplash</a>!</p>
-          <a class="btn btn-dark btn-xl js-scroll-trigger" href="#services">What We Offer</a>
+            <h2>Développeur web fullstack junior</h2>
+            <p class="lead mb-5">
+                    Je suis actuellement à la recherche d'une alternance en tant que développeur web fullstack. 
+                    Je débuterai en septembre un master "CEO Expertise Digitale" au sein de l'école <a href="https://www.eemi.com/la-formation/ceo-expertise-digitale/">EEMI</a> Paris.
+            </p>
+          <a class="btn btn-dark btn-xl js-scroll-trigger" href="#services">Voir les compétences</a>
         </div>
       </div>
     </div>
@@ -82,53 +84,148 @@
   <section class="content-section bg-primary text-white text-center" id="services">
     <div class="container">
       <div class="content-section-heading">
-        <h3 class="text-secondary mb-0">Services</h3>
-        <h2 class="mb-5">What We Offer</h2>
+        <h2 class="mb-5">Compétences</h2>
+       
       </div>
       <div class="row">
+
         <div class="col-lg-3 col-md-6 mb-5 mb-lg-0">
-          <span class="service-icon rounded-circle mx-auto mb-3">
-            <i class="icon-screen-smartphone"></i>
-          </span>
-          <h4>
-            <strong>Responsive</strong>
-          </h4>
-          <p class="text-faded mb-0">Looks great on any screen size!</p>
+            <div class="c100 p80 center">
+                <span>HTML</span>
+                <div class="slice">
+                    <div class="bar"></div>
+                    <div class="fill"></div>
+                </div>
+            </div>
+            <P>HTML5</P>
         </div>
+
         <div class="col-lg-3 col-md-6 mb-5 mb-lg-0">
-          <span class="service-icon rounded-circle mx-auto mb-3">
-            <i class="icon-pencil"></i>
-          </span>
-          <h4>
-            <strong>Redesigned</strong>
-          </h4>
-          <p class="text-faded mb-0">Freshly redesigned for Bootstrap 4.</p>
+            <div class="c100 p40 center">
+                <span>CSS</span>
+                <div class="slice">
+                    <div class="bar"></div>
+                    <div class="fill"></div>
+                </div>
+            </div>
+            <P>CSS3</P>
         </div>
-        <div class="col-lg-3 col-md-6 mb-5 mb-md-0">
-          <span class="service-icon rounded-circle mx-auto mb-3">
-            <i class="icon-like"></i>
-          </span>
-          <h4>
-            <strong>Favorited</strong>
-          </h4>
-          <p class="text-faded mb-0">Millions of users
-            <i class="fas fa-heart"></i>
-            Start Bootstrap!</p>
+
+        <div class="col-lg-3 col-md-6 mb-5 mb-lg-0">
+            <div class="c100 p55 center">
+                <span>JS</span>
+                <div class="slice">
+                    <div class="bar"></div>
+                    <div class="fill"></div>
+                </div>
+            </div>
+            <P>JavaScript E6</P>
         </div>
-        <div class="col-lg-3 col-md-6">
-          <span class="service-icon rounded-circle mx-auto mb-3">
-            <i class="icon-mustache"></i>
-          </span>
-          <h4>
-            <strong>Question</strong>
-          </h4>
-          <p class="text-faded mb-0">I mustache you a question...</p>
+
+        <div class="col-lg-3 col-md-6 mb-5 mb-lg-0">
+            <div class="c100 p65 center">
+                <span>ANGULAR</span>
+                <div class="slice">
+                    <div class="bar"></div>
+                    <div class="fill"></div>
+                </div>
+            </div>
+            <P>Angular JS 1.6</P>
         </div>
       </div>
+
+      <div class="row">
+            <div class="col-lg-3 col-md-6 mb-5 mb-lg-0">
+                <div class="c100 p75 center">
+                    <span>LARAVEL</span>
+                    <div class="slice">
+                        <div class="bar"></div>
+                        <div class="fill"></div>
+                    </div>
+                </div>
+                <P>Laravel 5.4</P>
+            </div>
+            <div class="col-lg-3 col-md-6 mb-5 mb-lg-0">
+                <div class="c100 p80 center">
+                    <span>PHP</span>
+                    <div class="slice">
+                        <div class="bar"></div>
+                        <div class="fill"></div>
+                    </div>
+                </div>
+                <P>PHP 5/7</P>
+            </div>
+
+            <div class="col-lg-3 col-md-6 mb-5 mb-lg-0">
+                <div class="c100 p80 center">
+                    <span>SQL</span>
+                    <div class="slice">
+                        <div class="bar"></div>
+                        <div class="fill"></div>
+                    </div>
+                </div>
+                <P>MYSQL</P>
+            </div>
+            <div class="col-lg-3 col-md-6 mb-5 mb-lg-0">
+                <div class="c100 p30 center">
+                    <span>SYMFONY</span>
+                    <div class="slice">
+                        <div class="bar"></div>
+                        <div class="fill"></div>
+                    </div>
+                </div>
+                <P>Symfony 4</P>
+            </div>
+            
+        </div>
+      <div class="row">
+            <div class="col-lg-3 col-md-6 mb-5 mb-lg-0">
+                <div class="c100 p30 center">
+                    <span>JAVA</span>
+                    <div class="slice">
+                        <div class="bar"></div>
+                        <div class="fill"></div>
+                    </div>
+                </div>
+                <P>Java</P>
+            </div>
+
+            <div class="col-lg-3 col-md-6 mb-5 mb-lg-0">
+                <div class="c100 p90 center">
+                    <span>MSSQL</span>
+                    <div class="slice">
+                        <div class="bar"></div>
+                        <div class="fill"></div>
+                    </div>
+                </div>
+                <P>Microsoft Server SQL</P>
+            </div>
+
+            <div class="col-lg-3 col-md-6 mb-5 mb-lg-0">
+                <div class="c100 p90 center">
+                    <span>WP</span>
+                    <div class="slice">
+                        <div class="bar"></div>
+                        <div class="fill"></div>
+                    </div>
+                </div>
+                <P>WordPress / WooCommerce</P>
+            </div>
+            <div class="col-lg-3 col-md-6 mb-5 mb-lg-0">
+                <div class="c100 p70 center">
+                    <span>GIT</span>
+                    <div class="slice">
+                        <div class="bar"></div>
+                        <div class="fill"></div>
+                    </div>
+                </div>
+                <P>Git avec Git Flow</P>
+            </div>
+        </div>
     </div>
   </section>
 
-  <!-- Callout -->
+  {{-- <!-- Callout -->
   <section class="callout">
     <div class="container text-center">
       <h2 class="mx-auto mb-5">Welcome to
@@ -136,14 +233,14 @@
         next website!</h2>
       <a class="btn btn-primary btn-xl" href="https://startbootstrap.com/template-overviews/stylish-portfolio/">Download Now!</a>
     </div>
-  </section>
+  </section> --}}
 
   <!-- Portfolio -->
   <section class="content-section" id="portfolio">
     <div class="container">
       <div class="content-section-heading text-center">
         <h3 class="text-secondary mb-0">Portfolio</h3>
-        <h2 class="mb-5">Recent Projects</h2>
+        <h2 class="mb-5">Projet Récent</h2>
       </div>
       <div class="row no-gutters">
         <div class="col-lg-6">
@@ -197,18 +294,39 @@
   <!-- Call to Action -->
   <section class="content-section bg-primary text-white">
     <div class="container text-center">
-      <h2 class="mb-4">The buttons below are impossible to resist...</h2>
-      <a href="#" class="btn btn-xl btn-light mr-4">Click Me!</a>
-      <a href="#" class="btn btn-xl btn-dark">Look at Me!</a>
+      <h2 class="mb-4">Me Contacter</h2>
+      <form>
+        <div class="form-row">
+          <div class="col">
+            <input type="text" class="form-control" placeholder="Nom">
+          </div>
+          <div class="col">
+            <input type="text" class="form-control" placeholder="Prenom">
+          </div>
+        </div>
+        <br>
+        <div class="form-row">
+          <div class="col">
+            <input type="text" class="form-control" placeholder="Email">
+          </div>
+          <div class="col">
+            <input type="text" class="form-control" placeholder="Telephone">
+          </div>
+        </div>
+        <br>
+        <textarea name="sujet" id="sujet" cols="100" rows="10" placeholder="Votre message ici"></textarea>
+        <br>
+        <button type="button" class="btn btn-primary btn-lg">Valider</button>
+      </form>
     </div>
   </section>
 
   <!-- Map -->
   <section id="contact" class="map">
-    <iframe width="100%" height="100%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=Twitter,+Inc.,+Market+Street,+San+Francisco,+CA&amp;aq=0&amp;oq=twitter&amp;sll=28.659344,-81.187888&amp;sspn=0.128789,0.264187&amp;ie=UTF8&amp;hq=Twitter,+Inc.,+Market+Street,+San+Francisco,+CA&amp;t=m&amp;z=15&amp;iwloc=A&amp;output=embed"></iframe>
+        <iframe width="100%" height="100%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2623.5063149844223!2d2.2277152159398903!3d48.886687179290256!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e664e37303283b%3A0x791d93956456d156!2s88+Avenue+du+G%C3%A9n%C3%A9ral+de+Gaulle%2C+92800+Puteaux!5e0!3m2!1sen!2sfr!4v1561725944621!5m2!1sen!2sfr" allowfullscreen></iframe>
     <br />
     <small>
-      <a href="https://maps.google.com/maps?f=q&amp;source=embed&amp;hl=en&amp;geocode=&amp;q=Twitter,+Inc.,+Market+Street,+San+Francisco,+CA&amp;aq=0&amp;oq=twitter&amp;sll=28.659344,-81.187888&amp;sspn=0.128789,0.264187&amp;ie=UTF8&amp;hq=Twitter,+Inc.,+Market+Street,+San+Francisco,+CA&amp;t=m&amp;z=15&amp;iwloc=A"></a>
+      <a href="https://www.google.fr/maps/place/88+Avenue+du+G%C3%A9n%C3%A9ral+de+Gaulle,+92800+Puteaux/@48.8866872,2.2277152,17z/data=!3m1!4b1!4m5!3m4!1s0x47e664e37303283b:0x791d93956456d156!8m2!3d48.8866872!4d2.2299039"></a>
     </small>
   </section>
 
@@ -217,22 +335,22 @@
     <div class="container">
       <ul class="list-inline mb-5">
         <li class="list-inline-item">
-          <a class="social-link rounded-circle text-white mr-3" href="#">
-            <i class="icon-social-facebook"></i>
+          <a class="social-link rounded-circle text-white mr-3" href="https://www.linkedin.com/in/christian-oupoh-bb8207129/">
+            <i class="icon-social-linkedin"></i>
           </a>
         </li>
-        <li class="list-inline-item">
+        {{-- <li class="list-inline-item">
           <a class="social-link rounded-circle text-white mr-3" href="#">
             <i class="icon-social-twitter"></i>
           </a>
-        </li>
+        </li> --}}
         <li class="list-inline-item">
-          <a class="social-link rounded-circle text-white" href="#">
+          <a class="social-link rounded-circle text-white" href="https://github.com/chryskablan">
             <i class="icon-social-github"></i>
           </a>
         </li>
       </ul>
-      <p class="text-muted small mb-0">Copyright &copy; Your Website 2019</p>
+      <p class="text-muted small mb-0">Copyright &copy; Christian OUPOH 2019</p>
     </div>
   </footer>
 
